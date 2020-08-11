7bbcb8bb1e267244307632e3dea06634a8f3ab0e
NAME <gbgo77@naver.com>
2020-08-11 19:04:32 +0900

    install bcrypt, save이전에 입력 pw 암호화 할 수 있도록 userSchema.pre 정의

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+const saltRounds = 10
+
 
 const userSchema = mongoose.Schema({
     name : {
@@ -28,6 +31,26 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: {
         type: Number
+    }
+})
+
+// 여기서 next는 user.save 의미.. 인자 안넣으면 error 발생 함...
+userSchema.pre('save', function( next ){
+    var user = this;
+
+    if(user.isModified('password')){
+    // 암호화
+    bcrypt.genSalt(saltRounds, function(err, salt){
+        if(err)
+            return next(err);
+        
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err)
+                    return next(err);
+                    user.password = hash;
+                    next();
+            })
+        })
     }
 })
 
