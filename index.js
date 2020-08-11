bad3a9b4f8311c17420b42a45eb477affa1f8e8e
NAME <gbgo77@naver.com>
2020-08-11 18:16:44 +0900

    db connect info separate and .. dev config ignore add

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 5000
 const mongoose = require("mongoose")
 const bodyParser = require('body-parser');
+const config = require('./config/key');
 const { User } = require("./models/User");
 
 // application/x-www-form-unlencoded
@@ -10,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 // application/json
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb+srv://taesan:0000@nodeproject.ennhq.mongodb.net/test?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
