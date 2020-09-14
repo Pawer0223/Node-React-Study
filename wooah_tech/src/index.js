57d697d75271391cbd5ded51306c06ac48c3e32c
NAME <gbgo77@naver.com>
2020-09-14 11:26:39 +0900

    commit

--- wooah_tech/src/index.js
+++ wooah_tech/src/index.js
@@ -1,22 +1,5 @@
-/* @jsx createElement */
+import React from 'react'
+import ReactDOM from 'react-dom';
+import App from "./App"
 
-import { createElement, render } from "./tiny-react";
-
-function Hello(props) {
-  return <li className="item">{props.label}</li>;
-}
-
-function App() {
-  return (
-    <div>
-      <h1>hello world</h1>
-      <ul className="board" onClick={() => null}>
-        <Hello label="Hello" />
-        <Hello label="World" />
-        <Hello label="React" />
-      </ul>
-    </div>
-  );
-}
-
-render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
